9794e0e23cd9a7b039744a3968ec6b1096df964e
Fuyukiri <ns34gp@hotmail.com>
2023-01-14 17:37:58 -0800

    week3 part 1 milestone 3&4

--- 2020/week3/inspect-fds/src/process.rs
+++ 2020/week3/inspect-fds/src/process.rs
@@ -1,6 +1,5 @@
 use crate::open_file::OpenFile;
-#[allow(unused)] // TODO: delete this line for Milestone 3
-use std::fs;
+use std::fs::{self, read_dir};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Process {
@@ -19,16 +18,28 @@ impl Process {
     /// information will commonly be unavailable if the process has exited. (Zombie processes
     /// still have a pid, but their resources have already been freed, including the file
     /// descriptor table.)
-    #[allow(unused)] // TODO: delete this line for Milestone 3
     pub fn list_fds(&self) -> Option<Vec<usize>> {
-        // TODO: implement for Milestone 3
-        unimplemented!();
+        let path = format!("/proc/{}/fd", self.pid);
+        let read_dir = read_dir(path).ok()?;
+        let mut fd_list = vec![];
+
+        for dir_entry in read_dir {
+            let file_name = 
+            dir_entry.ok()?
+            .file_name()
+            .into_string()
+            .ok()?
+            .parse::<usize>()
+            .ok()?;
+            fd_list.push(file_name);
+        }
+
+        Some(fd_list)
     }
 
     /// This function returns a list of (fdnumber, OpenFile) tuples, if file descriptor
     /// information is available (it returns None otherwise). The information is commonly
     /// unavailable if the process has already exited.
-    #[allow(unused)] // TODO: delete this line for Milestone 4
     pub fn list_open_files(&self) -> Option<Vec<(usize, OpenFile)>> {
         let mut open_files = vec![];
         for fd in self.list_fds()? {
@@ -39,6 +50,25 @@ impl Process {
 
     pub fn print(&self) {
         println!("========== \"{}\" (pid {}, ppid {}) ==========", self.command, self.pid, self.ppid);
+        match self.list_open_files() {
+            None => println!(
+                "Warning: could not inspect file descriptors for this process! \
+                    It might have exited just as we were about to look at its fd table, \
+                    or it might have exited a while ago and is waiting for the parent \
+                    to reap it."
+            ),
+            Some(open_files) => {
+                for (fd, file) in open_files {
+                    println!(
+                        "{:<4} {:<15} cursor: {:<4} {}",
+                        fd,
+                        format!("({})", file.access_mode),
+                        file.cursor,
+                        file.colorized_name(),
+                    );
+                }
+            }
+        }
     }
 }
 
@@ -57,11 +87,13 @@ mod test {
     fn test_list_fds() {
         let mut test_subprocess = start_c_program("./multi_pipe_test");
         let process = ps_utils::get_target("multi_pipe_test").unwrap().unwrap();
+
+        // For milestone 3, the fd may vary due to env
         assert_eq!(
             process
                 .list_fds()
                 .expect("Expected list_fds to find file descriptors, but it returned None"),
-            vec![0, 1, 2, 4, 5]
+            vec![0, 1, 2, 4, 5, 19, 20, 21]
         );
         let _ = test_subprocess.kill();
     }
