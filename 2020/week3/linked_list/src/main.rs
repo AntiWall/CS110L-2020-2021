bcc1659e8ca06dbdb3f0c6a20cb810bb82e6fe52
Fuyukiri <ns34gp@hotmail.com>
2023-01-15 21:34:04 -0800

    finish week3

--- 2020/week3/linked_list/src/main.rs
+++ 2020/week3/linked_list/src/main.rs
@@ -16,7 +16,59 @@ fn main() {
     println!("{}", list.to_string()); // ToString impl for anything impl Display
 
     // If you implement iterator trait:
-    //for val in &list {
-    //    println!("{}", val);
-    //}
+    for val in &list {
+       println!("{}", val);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_clone() {
+        let mut list: LinkedList<u32> = linked_list::LinkedList::new();
+        for i in 1..10 {
+            list.push_front(i);
+        }
+        let clone_list = list.clone();
+        list.pop_front();
+        assert_eq!(clone_list.get_size(), 9);
+        assert_eq!(list.get_size(), 8);
+    }
+
+    #[test]
+    fn test_partial_eq() {
+        let mut list: LinkedList<u32> = linked_list::LinkedList::new();
+        for i in 1..10 {
+            list.push_front(i);
+        }
+        let mut other_list: LinkedList<u32> = linked_list::LinkedList::new();
+        for i in 1..10 {
+            other_list.push_front(i);
+        }
+        assert!(list == other_list);
+    }
+
+    #[test]
+    fn test_not_partial_eq() {
+        let mut list: LinkedList<u32> = linked_list::LinkedList::new();
+        for i in 1..10 {
+            list.push_front(i);
+        }
+        let mut other_list: LinkedList<u32> = linked_list::LinkedList::new();
+        for i in 1..8 {
+            other_list.push_front(i);
+        }
+        assert!(list != other_list);
+    }
+
+    #[test]
+    fn test_compute_norm() {
+        let mut list: LinkedList<f64> = linked_list::LinkedList::new();
+        for i in 3..5 {
+            list.push_front(i as f64);
+        }
+        assert_eq!(5.0, linked_list::ComputeNorm::compute_norm(&list));
+    }
 }
